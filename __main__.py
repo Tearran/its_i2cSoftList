eab72de5e9af8dde0e8640474158b44684b0a27b
Joseph Turner <Tearran@users.noreply.github.com>
2022-10-04 15:27:07 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,13 +5,14 @@
 
 import sys, os, sqlite3
 
-os.system("clear")
+#os.system("clear")
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 from smbus2 import SMBus
 
-def scan(bus_num, start=0x03, end=0x78):
-
+def scan(bus_num, datatype, start=0x03, end=0x78):
+    if datatype == "cli":
+        os.system("clear") 
     path=os.path.dirname(__file__)
     its_list=os.listdir(path)
     its_self=os.path.basename(__file__)
@@ -32,11 +33,11 @@ def scan(bus_num, start=0x03, end=0x78):
             except:         
                 print("\tTry: sudo raspi-config nonint do_i2c 0")
                 sys.exit()
-          
-    print("I2C bus       : " + str(bus_num))
-    print("Start address : " + hex(start))
-    print("End address   : " + hex(end) + "\n")
-    
+#          
+#    print("I2C bus       : " + str(bus_num))
+#    print("Start address : " + hex(start))
+#    print("End address   : " + hex(end) + "\n")
+#    
     for i in range(start, end):
         val = 1
         try:
@@ -47,30 +48,44 @@ def scan(bus_num, start=0x03, end=0x78):
             c = conn.cursor()
             c.execute("SELECT * FROM its_i2c")
             rows = c.fetchall() 
-            #print(f"this is a comlete row--- {rows} --- ") 
-                  
+            #print(f"\n\nthis is a comlete row \n####################\n{rows}\n####################\n") 
+            #print(str(datatype))
+            #sys.exit()      
             if val != 5:    # No device
                 for row in rows:
                     if val == 1 :                                                 
                         if row[1] == hex(i) and row[5] != "legacy": 
                             if row[5] == "development" or row[5] != "legacy" :
-                                print(f'{row[1]}:\n  Found: {row[3]}\n  Type: {row[4]}\n  URL:{row[5]}\n  ')
-                        
                                 
+                                if datatype == "cli":                                                                      
+                                    report = (f'Device @ {str(row[1])}\n\tName:\t{row[3]}\n\tType:\t{row[4]}\n\tURL:\t{row[5]}\n')                                   
+                                
+                                elif datatype == "dict":
+                                    report = (f'i2c : {str(row[1])}, Name : {row[3]}, Type : {row[4]}, URL : {row[5]}')                                                        
+                                
+                                elif datatype == "csv":
+                                    report = (f'{str(row[1])} , {row[3]} , {row[4]} , {row[5]}')                            
+                                
+                                else:
+                                    report = ("Usage : i2c-scanner.py <datatype>\n\t cli\n\t csv\n\t dict")                                    
+                                    print(f'{report}')
+                                    sys.exit() 
+                                                                
+                                print(f'{report}')
               
     conn.close
                
 if __name__ == "__main__":
     args = sys.argv
     if len(args) < 2:
-        print("Usage : i2c-scanner.py <bus>\n")
-                 
+        print("Usage : i2c-scanner.py <datatype>\n")
+             
     else:
-      if int(args[1]) != 1:
-        print("Untested Bus:")
-        print(f"Try Default Bus:")
+      if args[1] == "data":
+        scan(int(1), "data")
+        #print(f"Try Default Bus 1:")
         pass     
       else:
-        scan(int(args[1]))
+        scan(int(1), args[1])
     sys.exit()       
      
